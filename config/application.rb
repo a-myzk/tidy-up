cf4fdf9f9c68cd14106fe859cc6e440c98aacf3a
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-13 10:31:37 +0900

    [Update]不要なファイルが自動生成されないよう設定 #1

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module TidyUp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
