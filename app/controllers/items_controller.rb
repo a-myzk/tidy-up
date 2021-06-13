a162dbbe50d78a53b5b74757e60aaff70ace55f9
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-13 19:00:37 +0900

    [Update]モノ登録フォーム作成 #5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,23 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path, notice: "登録しました！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :comment)
+  end
+
 end
