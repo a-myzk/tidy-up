512df063a518c589a3aba6eb3bd61aefacea75aa
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-14 14:19:14 +0900

    [Update]画像のサイズ指定 #5

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
+  include CarrierWave::MiniMagick
   storage :file
-  # storage :fog
+  process :resize_to_limit => [200, 200]
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
