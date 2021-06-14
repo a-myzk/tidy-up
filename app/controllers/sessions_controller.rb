25745e420248fd4162eba5348774380552914c48
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-14 15:53:21 +0900

    [Add]ログアウト機能作成 #7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
+  end
+
 end
