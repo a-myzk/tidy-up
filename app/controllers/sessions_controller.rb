45408bacbd904b06b5b4cc6f0f75e32290336f4f
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-14 15:50:39 +0900

    [Add]ログイン機能作成 #7

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+
+end
