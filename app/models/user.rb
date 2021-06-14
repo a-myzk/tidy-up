cb2e8216932ea81f4d34316af9bc2990d1bfa0a6
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-14 15:34:14 +0900

    [Update]セキュアパスワードの設定 #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
