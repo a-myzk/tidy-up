3dace7d1b158291b6123a77c5ad58a55923b1813
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-15 11:10:41 +0900

    [Update]不要なコメントアウト削除 #9

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,9 @@ class Category < ApplicationRecord
 
   enum name:{
     選択してください: 0,
-    衣料品: 1, #衣料品
-    日用品: 2, #日用品
-    書籍: 3, #書籍
-    その他: 4 #その他
+    衣料品: 1,
+    日用品: 2,
+    書籍: 3,
+    その他: 4
   }
 end
