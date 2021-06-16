f6f3f50098e6134b3ee1021883eaf980912137b5
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-16 15:02:57 +0900

    [Update]診断結果のデフォルト値を削除し3つの診断結果が正しく分岐されるよう修正 #11

--- db/migrate/20210615084844_change_column_default_to_clothes_diagnoses.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnDefaultToClothesDiagnoses < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default :clothes_diagnoses, :one_answer, from: nil, to: 50
-    change_column_default :clothes_diagnoses, :two_answer, from: nil, to: 50
-    change_column_default :clothes_diagnoses, :three_answer, from: nil, to: 50
-    change_column_default :clothes_diagnoses, :four_answer, from: nil, to: 50
-    change_column_default :clothes_diagnoses, :five_answer, from: nil, to: 50
-  end
-end
