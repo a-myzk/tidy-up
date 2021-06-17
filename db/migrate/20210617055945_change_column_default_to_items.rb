ac4c183ae4f9037af25c0356401dc0e958e93766
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-17 15:12:20 +0900

    [Add]モノ登録編集フォームにステータス登録機能を追加 #13

---
+++ db/migrate/20210617055945_change_column_default_to_items.rb
@@ -0,0 +1,5 @@
+class ChangeColumnDefaultToItems < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :items, :status, from: nil, to: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_125826) do
+ActiveRecord::Schema.define(version: 2021_06_17_055945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_125826) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "other_diagnoses", force: :cascade do |t|
