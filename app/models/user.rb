f32ac63ff1efd9716f0b4169a149ee720b4e3259
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-20 22:45:59 +0900

    [Update]devise導入準備 #19

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
