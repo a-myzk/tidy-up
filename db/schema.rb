39b3398a6359ee9e32d4af3ba63ece4c899fbae1
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-21 22:33:39 +0900

    [Add]itemテーブルにcategoryカラム追加 #25

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_140057) do
+ActiveRecord::Schema.define(version: 2021_06_21_133250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_140057) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.bigint "user_id"
+    t.integer "category"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
