720cf79a4ef818c607ba91a2d33d64f1b21c41de
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-22 15:43:46 +0900

    [Add]UserFactory作成 #33

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,42 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'test_name1' }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/images/camera.jpg'))}
+    comment { 'test_comment1' }
+    status { 1 }
+    category { 1 }
+  end
+
+  factory :item do, class: Item do
+    name { 'test_name2' }
+    image { 'test_' }
+    comment { 'test_comment2' }
+    status { 2 }
+    category { 2 }
+  end
+
+  factory :item do, class: Item do
+    name { 'test_name3' }
+    image { 'test_' }
+    comment { 'test_comment3' }
+    status { 3 }
+    category { 3 }
+  end
+
+
+  factory :item do, class: Item do
+    name { 'test_name4' }
+    image { 'test_' }
+    comment { 'test_comment4' }
+    status { 4 }
+    category { 4 }
+  end
+
+  factory :item do, class: Item do
+    name { 'test_name5' }
+    image { 'test_' }
+    comment { 'test_comment5' }
+    status { 0 }
+    category { 0 }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :admin_user do
+    name { 'test_user1' }
+    email  { 'test_email1@a.com' }
+    password { 'password1' }
+    password_confirmation { 'password1' }
+    admin { true }
+  end
+
+  factory :user, class: User do
+    name { 'test_user2' }
+    email  { 'test_email2@a.com' }
+    password { 'password2' }
+    password_confirmation { 'password2' }
+    admin { false }
   end
 end
