4f6cd1d7a5efea156ccbfd6c36514cd398f3e964
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-26 13:44:56 +0900

    [Fix]ユーザー編集時にパスワード未入力での編集ができなかったため修正 #65

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, on: :create, length: { maximum: 50 }
   before_validation { email.downcase! }
 
   def self.guest
