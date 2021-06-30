25e9138ffe91bda68eca786a09e6a4c4eefb9867
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-06-30 15:35:00 +0900

    [Update]診断の点数割り振りを修正

--- app/models/clothes_diagnosis.rb
+++ app/models/clothes_diagnosis.rb
@@ -5,5 +5,5 @@ class ClothesDiagnosis < ApplicationRecord
   enum two_answer: {bad_two: 0, normal_two: 1, good_two: 2}
   enum three_answer: {bad_three: 0, normal_three: 1, good_three: 2}
   enum four_answer: {bad_four: 0, normal_four: 1, good_four: 2}
-  enum five_answer: {bad_five: 0, normal_five: 1, good_five: 2}
+  enum five_answer: {bad_five: 2, normal_five: 1, good_five: 0}
 end
